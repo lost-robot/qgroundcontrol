d69469a22f998e8b9e83f63bee5f3d4a8b138f2f
nathan <nathan.donahue@gmail.com>
2025-05-19 14:57:01 -0600

    update docker for uid

--- deploy/docker/run-docker-ubuntu.sh
+++ deploy/docker/run-docker-ubuntu.sh
@@ -9,8 +9,16 @@ IMAGE_NAME="qgc-ubuntu-docker"
 SOURCE_DIR="$(pwd)"
 BUILD_DIR="${SOURCE_DIR}/build"
 
+export CONTAINER_GID=$(id -g)
+export CONTAINER_UID=$(id -u)
+export CONTAINER_USER=$USER
+
 # Build the Docker image
-docker build --file "${DOCKERFILE_PATH}" -t "${IMAGE_NAME}" "${SOURCE_DIR}"
+docker build --file "${DOCKERFILE_PATH}" \
+    --build-arg CONTAINER_USER=$CONTAINER_USER \
+    --build-arg CONTAINER_UID=$CONTAINER_UID \
+    --build-arg CONTAINER_GID=$CONTAINER_GID \
+    -t "${IMAGE_NAME}" "${SOURCE_DIR}"
 
 # Run the Docker container with necessary permissions and volume mounts
 docker run \
---
+++ notes.md
@@ -0,0 +1,17 @@
+# build
+cd repo
+mkdir build
+./deploy/docker/run-docker-ubuntu.sh
+
+- made edits to Dockerfile and build command to ensure user id and group id were consistent with host
+
+
+
+<!-- docker run --rm \
+  --privileged \
+  --device /dev/fuse \
+  -u $(id -u):$(id -g) \
+  -v ${PWD}:/project/source \
+  -v ${PWD}/build:/project/build \
+  qgc-linux-docker -->
+
